8f55d2c0dd0cba7027205e851f8cdc70a353d83b
Sami Toukola <toukolasami@gmail.com>
2019-10-01 08:58:55 +0300

    IfTask2 Created

---
+++ IfTask1/IfTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää käyttäjän syöttämästä luvusta," +
+                " onko se positiivinen vai negatiivinen!");
+            Console.Write("Syötä kokonaisluku: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if( userNumber == 0)
+            {
+                Console.WriteLine($"Luku {userNumber} on nolla!");
+            }
+            else if (userNumber > 0)
+            {
+                Console.WriteLine($"Luku {userNumber} on positiivinen!");
+            }
+            else
+            {
+                Console.WriteLine($"Luku {userNumber} on negatiivinen!");
+            }
+        }
+    }
+}
